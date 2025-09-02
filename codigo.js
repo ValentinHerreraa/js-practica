a932a4e3e42349d0eb24f5904e4ac4ffaf5e552c
vazx1 <valentinherreraprogramacion@gmail.com>
2025-09-02 19:50:22 -0300

    primer-dia-js

---
+++ codigo.js
@@ -0,0 +1,66 @@
+let prueba1 = "valen";
+//alert (nombre);
+
+//TIPOS DE DATOS (string "", numero = x, boolean = true or false)
+srting = "cadena de texto";
+number = 10;
+booleano = true, false ;
+
+//TIPOS DE DATOS ESPECIALES(undefined, null)
+let indefinido = 1;
+let nose = 'null';
+
+//NAN
+let numero1 = 2;
+let numero2 = "pedro";
+//alert (numero1 * numero2)
+
+//VARIABLES MULTIPLES: consiste en separar muchas variables mediante una coma.
+let numero = 2, nuemero1 = 2;
+
+//PRUEBAS CON PROMPT
+//let nombre = prompt("que edad tenes?");
+//alert (nombre);
+
+//OPERADORES DE ASIGNACION(=, +=, -=, /=, %=)
+let number1 = 50;
+number1 %= 7;
+
+//alert(number1);
+
+//OPERADORES ARIMETICOS: (--, ++, +, -, /, %, **x, -numero1(lo pone en negativo))
+let number2 = 10;
+let number3 = 3;
+
+number2++;
+resultado = number2; 
+//alert(resultado);
+
+//CONCATENACION (+): concatenacion normal
+saludo = "hola pedro! ";
+pregunta = "como estas?";
+
+amigos = saludo + pregunta;
+
+//alert(amigos);
+
+//FORZAR UN STRING: ponemos al principio unas comillas ya que todo lo toma como un string
+let uno = 1;
+let dos = 2;
+
+frase = "" + uno + dos
+alert(frase)
+
+//CONCAT: solo sirve para las cadenas de texto.
+ let hola = "que haces capo "
+ let chau = "nos vemos crack"
+
+ esoo = hola.concat(chau)
+
+ alert(esoo)
+
+ //${}
+ let nombree = "lucas dalto"
+
+ let frases = `soy ${nombree} y estoy caminando`
+ alert (frases)
