c7cb4d8f540dd6ef348278841470d106c481ce92
vazx1 <valentinherreraprogramacion@gmail.com>
2025-09-03 20:07:34 -0300

    segundo-capitulo-finalizado

--- segundo-capitulo.js
+++ segundo-capitulo.js
@@ -25,7 +25,7 @@ let frase = `El nombre de tu pc es: ${nombrepc}<br>;
 document.write(frase);*/
 
 //BUCLES E ITERACION
-//while
+//While
 numberwhile = 0
 /*while (numberwhile <= 10){
 
@@ -33,7 +33,7 @@ numberwhile = 0
 };
 document.write(numberwhile);*/
 
-//do while
+//Do While
 /*do{
     document.write(numberwhile);
     numberwhile ++;
@@ -56,15 +56,102 @@ while(diez <= 20){
     if(diez === 15){
         continue;
     }
-    document.write(diez + "<br>")
-}*/
+    document.write(diez + "<br>");
+};*/
 
-/*bucles for: esta conmpuesto de tres partes la cuales son |-----> Declaracion e inicializacion
+/* For: esta conmpuesto de tres partes la cuales son |-----> Declaracion e inicializacion
                                                            |-----> condicion
                                                            |-----> Las iteramos, aumento o decremento*/
-for(i = 0; i < 10; i++){
-    document.write(i + "<br>")
-} 
+/*for(i = 0; i < 10; i++){
+    document.write(i + "<br>");
+}; 
 for(i = 0; i < 20; i++){
-    document.write(i)
+    document.write(i);
+};*/
+
+//For In: te muestra en que indice esta cada elemento del array, no su valor.
+ let animales = ["gato", "perro", "tiranosaurio"];
+
+ /*for (animal in animales){
+    document.write(animal + "<br>");
+ };
+
+ document.write("<br>");*/
+
+ //For Of: te mustre ekl elemenot en si del array, no en la posicion que se encuntra.
+ /*for (animal of animales){
+    document.write(animal + "<br>");
+ };*/
+
+ //Label
+ 
+/* array1 = ["maria", "josefa", "roberto"]
+ array2 = ["pedro", "marcelo", array1, "josefina"]
+
+forRancio:
+ for (let array in array2){
+    if(array == 2){
+
+        for(let array of array1){
+            document.write(array + "<br>")
+            break forRancio;
+        }
+
+    } else{
+        document.write(array2[array] + "<br>")
+    }
 }
+    
+
+document.write("<br>")
+
+ cantidad = ["manzana", "banana", "pera"]
+
+ for (frutas in cantidad){
+    document.write(cantidad[frutas]+ "<br>")
+ } */
+
+ //FUNCIONES: simplemente escribimos una sola vez el codigo y lo podemos volver a reutilizar.
+  /*function saludar (){
+    respuesta = prompt("Hola valen, como estuvo tu dia?")
+    if(respuesta == "bien"){
+        alert("me alegro")
+    } else{
+        alert("una pena")
+    }
+  }
+  saludar()*/
+
+  //return
+  let = numberrr = 10
+  function hello (){
+    if(numberrr > 0){
+        return "piola"
+    } else{
+        return"una macana"
+    }
+  }
+let mayor = hello()
+alert(mayor)
+
+//parametros
+function suma(num1, num2){
+   let result = num1 + num2
+   return result
+}
+alert(suma(1, 2))
+
+function saludo (nombre){
+    let frase = `hola ${nombre}! como estas?`
+    alert (frase)
+}
+saludo("pedro")
+
+//scope: el scope de las funciones es regional, variable que va dentro solo funciona para dentro de esa funcion y no la podemos utilizar en otra parte del codigo.
+
+//funciones flecha
+const saludar = nombre =>{
+    document.write("hola como estas?" + nombre)
+}
+saludar("valen")
+
